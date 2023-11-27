6a61d46299ce8e7a080c333d34de029791667c72
olehvikanov <olehvikanov04@gmail.com>
2023-11-27 20:58:15 +0300

    version 1.0

---
+++ const.py
@@ -0,0 +1,27 @@
+site_list = [
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-banco-espana/notas-banco-espana/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-banco-espana/intervenciones-publicas/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-banco-espana/articulos-entrevistas-alta-administracion/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/notas-prensa-bce/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/decisiones-politica-monetaria/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/resenas-las-reuniones-politica-monetaria/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/otras-decisiones-consejo-gobierno/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/mecanismo-unico-supervision-mus/",
+    "https://www.bde.es/wbe/en/noticias-eventos/otras-instituciones/mecanismo-unico-resolucion-mur/",
+    "https://www.bde.es/wbe/en/noticias-eventos/otras-instituciones/junta-riesgo-sistemico-jers/",
+    "https://www.bde.es/wbe/en/noticias-eventos/otras-instituciones/comite-supervision-bancaria-basilea-bcbs/",
+    "https://www.bde.es/wbe/en/noticias-eventos/otras-instituciones/autoridad-bancaria-europea-eba/",
+]
+
+speech_list = [
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-banco-espana/intervenciones-publicas/"
+]
+press_release_list = [
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-banco-espana/notas-banco-espana/",
+    "https://www.bde.es/wbe/en/noticias-eventos/actualidad-bce/notas-prensa-bce/",
+]
+
+DOCUMENT_TYPE = {
+    "SPEECH":speech_list,
+    "PRESS_RELEASE":press_release_list
+}
